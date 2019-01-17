3031da661fff16b2686e18006aa7eca8af3513de
PinqDev <Pokkepinguin_dkr@hotmail.com>
2019-01-17 22:12:59 +0100

    Add keybindingchanges

--- FS19_financialManager/scripts/fmMain.lua
+++ FS19_financialManager/scripts/fmMain.lua
@@ -7,6 +7,39 @@
 
 fmMain = {}
 
-function fmMain.function_name( ... )
-    -- body
+
+function fmMain:loadMap(...)
+    self.cont = 4
+    self.eventIdActive = ""
+	self.inputsActive = false
+	self.errorDisplayed = true
 end
+
+function fmMain:deleteMap()
+end
+
+function fmMain:mouseEvent(...)
+end
+
+function fmMain:keyEvent(...)
+fmMain
+
+function fmMain:update(dt)
+    if g_gui.currentGui == nil and g_currentMission.controlledVehicle == nil then -- only if no vehicle is entered or menu is up
+
+        if not self.inputsActive then -- register input events
+            _, self.eventIdActive = g_inputBinding:registerActionEvent(InputAction.fmHomeScreen, fmMain, fmMain.reduceSpeed, false, true, false, false)
+            self.inputsActive = true
+            print("Hello World i'm activated by pressing a key!")
+
+        end
+    else
+        self.inputsActive = false
+    end
+end
+
+function fmMain:draw()
+end
+
+print("  Loaded SpeedPlayer Mod...")
+addModEventListener(fmMain)
