0c6503b0926c44b1ab6e2e1b576337356679e2c8
PinqDev <Pokkepinguin_dkr@hotmail.com>
2019-01-17 23:00:15 +0100

    Develop

--- FS19_financialManager/scripts/fmMain.lua
+++ FS19_financialManager/scripts/fmMain.lua
@@ -8,10 +8,10 @@
 fmMain = {}
 
 function fmMain:loadMap(...)
-    self.cont = 4
+    --self.cont = 4
     self.eventIdActive = ""
 	self.inputsActive = false
-	self.errorDisplayed = true
+    --print("Hier zit ik in de loadmap")
 end
 
 function fmMain:deleteMap()
@@ -27,13 +27,23 @@ function fmMain:activateScreen()
 	print("Hello World i'm activated by pressing a key!")
 end
 
-function fmMain:update(dt)  
+function fmMain:update()  
+    --print("Ik zit nu in de update")
     if g_gui.currentGui == nil and g_currentMission.controlledVehicle == nil then -- only if no vehicle is entered or menu is up
-
+        print(self.inputsActive)        
         if not self.inputsActive then -- register input events
+            --print("Ik zit in de register input")
             _, self.eventIdActive = g_inputBinding:registerActionEvent(InputAction.fmHomeScreen, fmMain, fmMain.activateScreen, false, true, false, false)
-            self.inputsActive = true
+            --self.inputsActive = true
         end
+        
+
+        --local eventIdActive = self.eventIdActive
+        --print("Ik zit net onder de local ")
+
+        --g_inputBinding:setActionEventActive(eventIdActive, true)
+		--g_inputBinding:setActionEventTextVisibility(eventIdActive, true)
+
     else
         self.inputsActive = false
     end
