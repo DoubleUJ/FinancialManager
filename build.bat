8fd7acd72b8d7c4e857fa62783f14ad4a544f4fc
Wieger Jan Schaap <wjschaap@vacerra.eu>
2019-01-18 10:43:31 +0100

    Add build en deploy scripts

---
+++ build.bat
@@ -0,0 +1,2 @@
+del FS19_financialManager.zip
+"D:\Program Files\7-Zip\7z.exe" a -r FS19_financialManager.zip -w ./FS19_financialManager/* -mem=AES256
---
+++ deploy.bat
@@ -0,0 +1,2 @@
+@echo "mods"
+@xcopy FS19_financialManager.zip "D:\wjsch\Documents\My Games\FarmingSimulator2019\mods" /R/U/Y/Q
