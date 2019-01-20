45a8acdbf859a912e4fd085a8c2365b4b5c13d7b
Wieger Jan Schaap <wjschaap@vacerra.eu>
2019-01-20 23:13:46 +0100

    Added directory for screens

--- FS19_financialManager/scripts/fmMain.lua
+++ FS19_financialManager/scripts/fmMain.lua
@@ -23,24 +23,20 @@ function fmMain:keyEvent(...)
 end
 
 function fmMain:activateScreen()
-    print("Hello World i'm activated by pressing a key!")
-	fmMain.fmShowHomeScreen( self )
+    print("I'm activated Financial Manager by pressing a key!")
+	fmMain.fmShowHomeScreen(self)
 end
 
 function fmMain:update()  
     
     if g_gui.currentGui == nil and g_currentMission.controlledVehicle == nil then -- only if no vehicle is entered or menu is up
-           
         if not self.inputsActive then -- register input events    
             _, self.eventIdActive = g_inputBinding:registerActionEvent(InputAction.fmHomeScreen, fmMain, fmMain.activateScreen, false, true, false, false)
             self.inputsActive = true
         end
-      
         local eventIdActive = self.eventIdActive
-             
         g_inputBinding:setActionEventActive(eventIdActive, true)
         g_inputBinding:setActionEventTextVisibility(eventIdActive, true)
-        
     else
         self.inputsActive = false
     end
@@ -53,13 +49,12 @@ function fmMain.fmShowHomeScreen()
         print("g_FinancialManagerHomeScreen is nil")
         -- TODO variable verwijzing voor aanmaken screen implementeren.
         g_FinancialManagerHomeScreen = FinancialManagerHomeScreen:new() 
-        --g_gui:loadGui("financialManagerHomeScreen.xml", "financialManagerHomeScreen") --g_keyboardSteerMogliScreen
+        -- g_gui:loadGui("screens/fmHomeScreen.xml", "financialManagerHomeScreen") --g_keyboardSteerMogliScreen
     end
 end
-
 
 function fmMain:draw()
 end
 
-print("  Loaded Financial Manager by MaxAgrisim   ")
+print("  Loaded Financial Manager by MaxAgriSim   ")
 addModEventListener(fmMain)
