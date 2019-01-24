a44b02947dd9fbd913186d0a83b51f12f766924a
PinqDev <Pokkepinguin_dkr@hotmail.com>
2019-01-24 19:45:55 +0100

    Add Gui Lua

---
+++ FS19_financialManager/fmGUI.lua
@@ -0,0 +1,16 @@
+--
+-- Financial Manager Gui
+-- V0.0.0.1
+--
+-- @author MaxAgrisim
+--
+
+fmGui = {};
+
+local fmGui_mt = Class(fmGui, ScreenElement);
+
+function fmGui:new(target, custom_mt)
+    local self = ScreenElement:new(target, fmGui_mt);
+    self.returnScreenName = "";
+    return self;	
+end;
