f10fd9c93a459c96266cfce9c0d90fab001de10d
PinqDev <Pokkepinguin_dkr@hotmail.com>
2019-02-13 20:59:16 +0100

    add changelog

--- FS19_financialManager/source/loader.lua
+++ FS19_financialManager/source/loader.lua
@@ -4,6 +4,9 @@
 -- loader script for the mod
 --
 -- Copyright (c) MaxAgrisim, 2019
+--
+-- changelog
+-- 0.0.0.1      First implementation        DoubleU.J. / Pinqdev 
 
 local directory = g_currentModDirectory
 local modName = g_currentModName
