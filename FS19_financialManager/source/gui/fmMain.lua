eb82bf7017581a5220de4b82322732e4e5b4d70d
PinqDev <Pokkepinguin_dkr@hotmail.com>
2019-02-13 21:23:29 +0100

    add variables

--- FS19_financialManager/source/gui/fmMain.lua
+++ FS19_financialManager/source/gui/fmMain.lua
@@ -7,8 +7,19 @@
 
 fmMain = {}
 
+local financialManager_mt = Class(fmMain)
+
 -- Setting directories global
 
+function fmMain:new(i18n, modDirectory, gui, inputManager)
+	local self = setmetatable({}, financialManager_mt)
+
+	self.i18n = i18n
+	self.modDirectory = modDirectory
+	self.gui = gui
+	self.inputManager = inputManager
+	
+end
 
 
 function fmMain:delete()
