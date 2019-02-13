11ad4066d69b252eaef7cfbfed4c1ca66de37545
PinqDev <Pokkepinguin_dkr@hotmail.com>
2019-02-13 20:41:16 +0100

    Deleted all functions

--- FS19_financialManager/scripts/fmMain.lua
+++ FS19_financialManager/scripts/fmMain.lua
@@ -8,24 +8,14 @@
 fmMain = {}
 
 -- Setting directories global
-fmMain.directory = g_currentModDirectory
-fmMain.scripts = g_currentModDirectory .. "scripts/"
-fmMain.gui = g_currentModDirectory .. 'gui/'
-fmMain.images = g_currentModDirectory .. 'images/'
-fmMain.data = g_currentModDirectory .. 'data/'
 
--- Setting directories local
-local fmMainDirectory = g_currentModDirectory
-local fmMainGui = g_currentModDirectory .. 'gui/'
 
-source(Utils.getFilename("fmGUI.lua", fmMain.scripts))
 
 function fmMain:delete()
 end
 
-function fmMain:loadMap(name)
-    -- Set inputactive false    
-    self.inputsActive = false
+function fmMain:loadMap()
+
 end
 
 function fmMain:deleteMap()
@@ -38,41 +28,15 @@ function fmMain:keyEvent(...)
 end
 
 function fmMain:activateScreen()
-    -- print("Debug: Financialmanager - ActivateScreen");
-    -- print("Debug: I'm activated Financial Manager by pressing a key!")
-	fmMain.fmShowHomeScreen(self)
+	--fmMain.fmShowHomeScreen(self)
 end
 
-
-function fmMain:update(dt)  
-    
-    if g_gui.currentGui == nil and g_currentMission.controlledVehicle == nil then -- No vehicle is entered or menu is up
-        if not self.inputsActive then -- register input events    
-            _, self.eventIdActive = g_inputBinding:registerActionEvent(InputAction.fmHomeScreen, fmMain, fmMain.activateScreen, false, true, false, false)
-            self.inputsActive = true
-        end
-        local eventIdActive = self.eventIdActive
-        g_inputBinding:setActionEventActive(eventIdActive, true)
-        g_inputBinding:setActionEventTextVisibility(eventIdActive, true)
-    else
-        self.inputsActive = false
-    end
-end
-
-function fmMain:load(xmlFile)	
+function fmMain:load()	
 end;
 
 function fmMain:fmShowHomeScreen()
-    -- print("Debug: Financialmanager - fmShowHomeScreen")
 
-    -- Set Gui
-    local fmMain = fmGui:new()
-    g_gui:loadGui(fmMainGui .. "fmGui.xml", "fmGui", fmMain, false)
-    g_gui:showGui("fmGui")
 end
-
-function fmMain:guiClosed()
-end;
 
 function fmMain:draw()
 end
