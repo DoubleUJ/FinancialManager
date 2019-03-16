8cd120b289af33372d525a6e2a4f290fb034410c
PinqDev <Pokkepinguin_dkr@hotmail.com>
2019-03-16 14:15:32 +0100

    Change in get gui

--- FS19_financialManager/source/gui/fmMain.lua
+++ FS19_financialManager/source/gui/fmMain.lua
@@ -11,31 +11,41 @@ local financialManager_mt = Class(fmMain)
 
 -- Setting directories global
 
-function fmMain:new(i18n, modDirectory, gui)
+function fmMain:new(i18n, modDirectory, gui, inputManager, messageCenter, settingsModel)
 	local self = setmetatable({}, financialManager_mt)
 
 	self.i18n = i18n
 	self.modDirectory = modDirectory
 	self.gui = gui
 	self.inputManager = inputManager
+	self.messageCenter = messageCenter
+    self.settingsModel = settingsModel
 	
 	return self
 end
 
 function fmMain:delete()
+	if self.isClient then
+
+		self:unloadMenu()
+	end
 end
 
 function fmMain:load()
+	if self.isClient then
+
+	self:loadMenu()
+end
 end
 
 function fmMain:loadMenu()
 	--fmMain.fmShowHomeScreen(self)
-	local fmMainScreen = fmMainScreen:new(self.i18n, self.inputManager)
+	local fmMainScreenobject = fmMainScreen:new(self.i18n, self.inputManager)
 	
 	self.menu = fmMenu:new(self.i18n, self.inputManager)
 
 	local root = Utils.getFilename("resources/gui/", self.modDirectory)
-	self.gui:LoadGui(root .. "fmMainScreen.xml", "fmMainScreen", fmMainScreen, true)
+	self.gui:LoadGui(root .. "fmMainScreen.xml", "fmMainScreen", fmMainScreenobject, true)
 	self.gui:LoadGui(root .. "fmMenu.xml", "fmMenu", self.menu)
 end
 
@@ -45,9 +55,6 @@ end
 
 function fmMain:onToggleUI() 
 	g_gui:showGui("fmMenu")
-end
-
-function fmMain:update()
 end
 
 function fmMain:draw()
