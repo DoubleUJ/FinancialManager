e3089d7f94a8ea8b07c2fc035d1933ffe9fca74f
PinqDev <Pokkepinguin_dkr@hotmail.com>
2019-03-25 20:12:10 +0100

    Develop

--- FS19_financialManager/source/loader.lua
+++ FS19_financialManager/source/loader.lua
@@ -13,7 +13,7 @@ local directory = g_currentModDirectory
 source(Utils.getFilename("source/gui/hud/fmFinancialManagerUI.lua", directory)) -- Hier de algemene functies rondom de hoofd ui zetten
 source(Utils.getFilename("source/gui/frames/fmFinancialScreen.lua", directory)) -- Hier de functies van het "tabbed menu" zetten die financiele zaken afhandelen 
 
-source(Utils.getFilename("source/FinancialManager.lua", directory)) -- Algemene functies plaatsten als die nodig zijn en er zijn. 
+--source(Utils.getFilename("source/FinancialManager.lua", directory)) -- Algemene functies plaatsten als die nodig zijn en er zijn. 
 
 local FinancialManager
 
